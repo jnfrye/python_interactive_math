33a0c252698197635175b9fbe8b8b50e46ef90ac
Josh Frye <jfrye@uoregon.edu>
2016-10-21 13:01:40 -0700

    Create setup function and initial requirements file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='PythonInteractiveMath',
+    version='0.1',
+    description='Python package for writing interactive math documents.',
+    author='Joshua Frye',
+    author_email='josh.n.frye@gmail.com ',
+    packages=['py_interactive_math'],
+)

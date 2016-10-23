afe53d541cdb120005f8ee031c02d7c9561889cc
Josh Frye <jfrye@uoregon.edu>
2016-10-23 14:23:17 -0700

    Add attribute for content mathml tag

--- py_interactive_math/math/function.py
+++ py_interactive_math/math/function.py
@@ -3,3 +3,10 @@
 
 class Function:
     """Base class for mathematical functions."""
+    content_mathml_tag = ""
+    "Corresponding content-MathML tag text."
+
+    def __init__(
+            self,
+            *args: "Arbitrary number of function arguments",):
+        self.args = args
